f4927cc08b03ce027c84d1dffd5db8a5bc6992d9
helux <kled.master@gmail.com>
2024-08-04 01:38:27 +0600

    Пагинация главной страницы, шаблон пагинации скатал с оф сайта джанго

--- source/webapp/views/topics.py
+++ source/webapp/views/topics.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView, DetailView
 
 from webapp.forms.replies import ReplyForm
+from webapp.forms.search_form import SearchForm
 from webapp.forms.topics import TopicForm
 from webapp.models import Topic, Reply
 
@@ -12,6 +15,36 @@ from webapp.models import Topic, Reply
 class TopicListView(ListView):
     model = Topic
     template_name = 'topics/home.html'
+    paginate_by = 7
+
+    def dispatch(self, request, *args, **kwargs):
+        self.form = self.get_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        form = self.form
+        if form.is_valid():
+            return form.cleaned_data['search']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(
+                Q(name__contains=self.search_value) | Q(description__contains=self.search_value)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = self.form
+        if self.search_value:
+            context["search"] = urlencode({"search": self.search_value})
+            context["search_value"] = self.search_value
+        return context
 
 
 class TopicDetailView(DetailView):
