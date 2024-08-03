c1c54587310ad9af3e4ced6dff3de3e071ba6648
helux <kled.master@gmail.com>
2024-08-04 02:44:48 +0600

    Косметические исправления

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from accounts.views import RegistrationView
+from accounts.views import RegistrationView, ProfileView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
