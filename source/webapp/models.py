02520c733e4970443486cc8813ac5df3cd990b5c
helux <kled.master@gmail.com>
2024-08-03 16:18:24 +0600

    Модели и миграции

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name="topics",
+        on_delete=models.SET_DEFAULT,
+        default=1
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Reply(models.Model):
+    topic = models.ForeignKey(Topic, related_name='replies', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        related_name="comments",
+        on_delete=models.SET_DEFAULT,
+        default=1
+    )
+
+    def __str__(self):
+        return f'Reply by {self.author} on {self.topic.title}'
--- source/webapp/views/topics.py
+++ source/webapp/views/topics.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
+class TopicListView(ListView):
+    model = Topic
+    template_name = 'topics/home.html'
+
+
+class TopicDetailView(TemplateView):
+    template_name = "topics/detail_topic.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.topic = get_object_or_404(Topic, pk=self.kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['topic'] = self.topic
+        return context
+
+
+class TopicCreateView(LoginRequiredMixin, CreateView):
+    template_name = "topics/add_topic.html"
+    form_class = TopicForm
+
+    def form_valid(self, form):
+        project = get_object_or_404(Project, pk=self.kwargs['pk'])
+        topic = form.save(commit=False)
+        topic.project = project
+        topic.save()
+        return redirect("webapp:project_detail", pk=self.kwargs['pk'])
+
+
+class TopicUpdateView(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+    model = Topic
+    form_class = TopicForm
+    template_name = "topics/update_topic.html"
+    context_object_name = 'topic'
+    permission_required = "webapp.change_topic"
+
+    def get_success_url(self):
+        return reverse_lazy('webapp:project_detail', kwargs={'pk': self.object.project.pk})
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().user
+
+
+class TopicDeleteView(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
+    model = Topic
+    template_name = "topics/delete_topic.html"
+    context_object_name = 'topic'
+    permission_required = "webapp.delete_topic"
+
+    def get_success_url(self):
+        return reverse_lazy('webapp:project_detail', kwargs={'pk': self.object.project.pk})
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().user
